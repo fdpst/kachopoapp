5529e7a265b436ffc3da692ff475899892393c81
alejandrovelascofidiasprog <97974764+alejandrovelascofidiasprog@users.noreply.github.com>
2022-04-26 16:51:44 -0400

    implementado trancking autorization en ios

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,18 @@ import 'package:flutter/material.dart';
 
 import 'src/pages/tabs_pages.dart';
 import 'src/themes/tema.dart';
+import 'package:app_tracking_transparency/app_tracking_transparency.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void init() async {
+    final status = await AppTrackingTransparency.requestTrackingAuthorization();
+  }
+
   @override
   Widget build(BuildContext context) {
+    init();
     return MaterialApp(
       title: 'Material App',
       theme: miTema,
